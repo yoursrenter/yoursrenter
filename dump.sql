58fcdc296fa55bf092411c51209e5a7c2459de24
evgen7171 <48080044+evgen7171@users.noreply.github.com>
2019-10-30 18:04:26 +0300

    дамп бд (для "сырого" входа)

---
+++ dump.sql
@@ -0,0 +1,22 @@
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               5.7.27-log - MySQL Community Server (GPL)
+-- Операционная система:         Win32
+-- HeidiSQL Версия:              10.1.0.5464
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Экспортируемые данные не выделены.
+-- Экспортируемые данные не выделены.
+-- Экспортируемые данные не выделены.
+-- Экспортируемые данные не выделены.
+-- Экспортируемые данные не выделены.
+-- Экспортируемые данные не выделены.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
