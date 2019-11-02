6cdee4290046377b25c9653366d78cbcea34e5f9
evgen7171 <48080044+evgen7171@users.noreply.github.com>
2019-11-02 04:36:00 +0300

    вход на сайт
    + регистрация
    + логирование (вход)
    (- нет валидации для логирования)

---
+++ comand.pr/views/home/tenantsList.php
@@ -0,0 +1,196 @@
+<p class="title_tenant">Список арендаторов</p>
+<div class="filter__menu">
+    <a href="../yoursrenter/filter.html" class="filter__menu_arrow"><img src="../../public/img/arrow-down-b_icon-icons_13.png" alt="arrow"></a>
+    <h3 class="filter__menu_h">Фильтр</h3>
+    <div class="button_X">
+    <button class="filter__menu_button"><a class="delete_x" href="#">X</a> Арендатор</button>
+    <button class="filter__menu_button"><a class="delete_x" href="#">X</a> Объект</button>
+    </div>
+    <form class="filter__search">
+        <input class="filter__search_input" type="text" placeholder="Поиск"><a href="#"><img class="filter__search_loop" src="../../public/img/monotone_search_zoom_1.png" alt="loop"></a>
+    </form>
+</div>
+<table class="table-sort">
+    <thead class="table_header">
+    <tr>
+    <th class="table_header"><input class="table__checkbox" type="checkbox"></th>
+        <th class="table_header">Имя арендатора</th>
+        <th class="table_header">Номер договора аренды</th>
+        <th class="table_header">Дата договора аренды</th>
+        <th class="table_header">Дата акта приема-передач</th>
+        <th class="table_header">Дата окончания договора</th>
+        <th class="table_header">Арендуемая площадь (‎м²)</th>
+        <th class="table_header">Арендуемый объект</th>
+        <th class="table_header">Дата начисления аренды</th>
+        <th class="table_header">Сумма аренды    в месяц</th>
+        <th class="table_header">Обеспечительный платеж</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td> </td>
+        <td><img class="table_arrow" src="../../public/img/arrow-down-b_icon-icons_13.png" alt="arrow"> </td>
+        <td><img class="table_arrow" src="../../public/img/arrow-down-b_icon-icons_13.png" alt="arrow"> </td>
+        <td><img class="table_arrow" src="../../public/img/arrow-down-b_icon-icons_13.png" alt="arrow"> </td>
+        <td><img class="table_arrow" src="../../public/img/arrow-down-b_icon-icons_13.png" alt="arrow"> </td>
+        <td><img class="table_arrow" src="../../public/img/arrow-down-b_icon-icons_13.png" alt="arrow"> </td>
+        <td><img class="table_arrow" src="../../public/img/arrow-down-b_icon-icons_13.png" alt="arrow"> </td>
+        <td><img class="table_arrow" src="../../public/img/arrow-down-b_icon-icons_13.png" alt="arrow"> </td>
+        <td><img class="table_arrow" src="../../public/img/arrow-down-b_icon-icons_13.png" alt="arrow"> </td>
+        <td><img class="table_arrow" src="../../public/img/arrow-down-b_icon-icons_13.png" alt="arrow"> </td>
+        <td><img class="table_arrow" src="../../public/img/arrow-down-b_icon-icons_13.png" alt="arrow"> </td>
+    </tr>
+    <tr class="table__content">
+        <th class="table__content_info"><input class="table__checkbox" type="checkbox"></th>
+        <th class="table__content_info">ООО Ромашка</th>
+        <th class="table__content_info">СП-306СП-19</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">150 кв.м.</th>
+        <th class="table__content_info">Склад</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">200 000 руб.</th>
+        <th class="table__content_info">200 000 руб.</th>
+    </tr>
+    <tr class="add__info">
+        <th></th>
+        <th></th>
+        <th class="add__info_1">Телефон</th>
+        <th class="add__info_1">Електронный адрес</th>
+        <th class="add__info_1">Дата платежа</th>
+        <th class="add__info_1">Дата акта доступа</th>
+    </tr>
+    <tr class="add__info">
+        <th></th>
+        <th></th>
+        <th class="add__info_2">+7 000 000 00 00</th>
+        <th class="add__info_2">mail@inbox.ru</th>
+        <th class="add__info_2">99.99.9999</th>
+        <th class="add__info_2">99.99.9999</th>
+    </tr>
+    <tr class="table__content">
+        <th class="table__content_info"><input class="table__checkbox" type="checkbox"></th>
+        <th class="table__content_info">ООО Ромашка</th>
+        <th class="table__content_info">СП-306СП-19</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">150 кв.м.</th>
+        <th class="table__content_info">Склад</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">200 000 руб.</th>
+        <th class="table__content_info">200 000 руб.</th>
+    </tr>
+    <tr class="table__content">
+        <th class="table__content_info"><input class="table__checkbox" type="checkbox"></th>
+        <th class="table__content_info">ООО Ромашка</th>
+        <th class="table__content_info">СП-306СП-19</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">150 кв.м.</th>
+        <th class="table__content_info">Склад</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">200 000 руб.</th>
+        <th class="table__content_info">200 000 руб.</th>
+    </tr>
+    <tr class="table__content">
+        <th class="table__content_info"><input class="table__checkbox" type="checkbox"></th>
+        <th class="table__content_info">ООО Ромашка</th>
+        <th class="table__content_info">СП-306СП-19</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">150 кв.м.</th>
+        <th class="table__content_info">Склад</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">200 000 руб.</th>
+        <th class="table__content_info">200 000 руб.</th>
+    </tr>
+    <tr class="table__content">
+        <th class="table__content_info"><input class="table__checkbox" type="checkbox"></th>
+        <th class="table__content_info">ООО Ромашка</th>
+        <th class="table__content_info">СП-306СП-19</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">150 кв.м.</th>
+        <th class="table__content_info">Склад</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">200 000 руб.</th>
+        <th class="table__content_info">200 000 руб.</th>
+    </tr>
+    <tr class="table__content">
+        <th class="table__content_info"><input class="table__checkbox" type="checkbox"></th>
+        <th class="table__content_info">ООО Ромашка</th>
+        <th class="table__content_info">СП-306СП-19</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">150 кв.м.</th>
+        <th class="table__content_info">Склад</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">200 000 руб.</th>
+        <th class="table__content_info">200 000 руб.</th>
+    </tr>
+    <tr class="table__content">
+        <th class="table__content_info"><input class="table__checkbox" type="checkbox"></th>
+        <th class="table__content_info">ООО Ромашка</th>
+        <th class="table__content_info">СП-306СП-19</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">150 кв.м.</th>
+        <th class="table__content_info">Склад</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">200 000 руб.</th>
+        <th class="table__content_info">200 000 руб.</th>
+    </tr>
+    <tr class="table__content">
+        <th class="table__content_info"><input class="table__checkbox" type="checkbox"></th>
+        <th class="table__content_info">ООО Ромашка</th>
+        <th class="table__content_info">СП-306СП-19</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">150 кв.м.</th>
+        <th class="table__content_info">Склад</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">200 000 руб.</th>
+        <th class="table__content_info">200 000 руб.</th>
+    </tr>
+    <tr class="table__content">
+        <th class="table__content_info"><input class="table__checkbox" type="checkbox"></th>
+        <th class="table__content_info">ООО Ромашка</th>
+        <th class="table__content_info">СП-306СП-19</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">150 кв.м.</th>
+        <th class="table__content_info">Склад</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">200 000 руб.</th>
+        <th class="table__content_info">200 000 руб.</th>
+    </tr>
+    <tr class="table__content">
+        <th class="table__content_info"><input class="table__checkbox" type="checkbox"></th>
+        <th class="table__content_info">ООО Ромашка</th>
+        <th class="table__content_info">СП-306СП-19</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">150 кв.м.</th>
+        <th class="table__content_info">Склад</th>
+        <th class="table__content_info">99.99.9999</th>
+        <th class="table__content_info">200 000 руб.</th>
+        <th class="table__content_info">200 000 руб.</th>
+    </tr>
+
+    </tbody>
+</table>
+<div class="scroll_area">
+    <div class="scrollbar_track"></div>
+</div>
+
+<button class="btn_add">Добавить </button>
